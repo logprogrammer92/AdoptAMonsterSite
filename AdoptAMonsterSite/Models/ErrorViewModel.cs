f5534a9314310e68070e76e2ffc9c14a035690a5
Logan Nelson <Lnelson0915@gmail.com>
2025-09-30 16:50:01 -0700

    Add project files.

---
+++ AdoptAMonsterSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdoptAMonsterSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
