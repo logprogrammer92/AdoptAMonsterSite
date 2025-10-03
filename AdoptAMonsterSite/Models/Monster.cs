1f1e17bd27d56f21da6fb61a9955263028bacba3
ToastedToast0090 <toastedtoast0090@gmail.com>
2025-10-03 00:53:14 -0700

    Created Monster.cs and refactored User.cs

---
+++ AdoptAMonsterSite/Models/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdoptAMonsterSite.Models;
+
+public class Monster
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [Display (Name = "Monster Name")]
+    public string Name { get; set; }
+
+    [Display (Name = "Species")]
+    public string? Species { get; set; }
+
+    [Display(Name = "Description")]
+    public string? Description { get; set; }
+    
+    [Display (Name = "AdoptionFee")]
+    [DataType (DataType.Currency)]
+    public decimal? Price { get; set; }
+}
