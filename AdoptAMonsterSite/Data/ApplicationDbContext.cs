4ab56e0544308109e68f55a0b0f03b1482c645f7
ToastedToast0090 <toastedtoast0090@gmail.com>
2025-10-03 01:01:02 -0700

    Integrated Monster listings into ApplicationDbContext - Added DbSet<Monster> to track storefront inventory - Ensured ApplicationDbContext inherits from IdnetityDbContext<ApplicationUser> - Included OnModelCreating override with unique index on Monster.Name - Called base.OnModelCreating to preserve Identity schema setup
    This unifues identity and product listing models under a single DbContext

--- AdoptAMonsterSite/Data/ApplicationDbContext.cs
+++ AdoptAMonsterSite/Data/ApplicationDbContext.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AdoptAMonsterSite.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AdoptAMonsterSite.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    //Track monster listings
+    public DbSet<Monster> Monsters { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Monster>()
+            .HasIndex(m => m.Name)
+            .IsUnique();
     }
 }
