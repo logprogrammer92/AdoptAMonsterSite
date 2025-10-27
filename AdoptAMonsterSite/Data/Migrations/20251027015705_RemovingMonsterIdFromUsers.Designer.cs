5cdf8486548b4dc1a900be2c844ab88a760f9a23
Logan Nelson <Lnelson0915@gmail.com>
2025-10-26 20:24:29 -0700

    Changing users to remove the MonsterId and linked the Monster to the OwnerId. Added db migration scripts to make sure that the changes to the models matches the database.

--- AdoptAMonsterSite/Data/Migrations/20251027015705_RemovingMonsterIdFromUsers.Designer.cs
+++ AdoptAMonsterSite/Data/Migrations/20251027015705_RemovingMonsterIdFromUsers.Designer.cs
@@ -4,6 +4,7 @@ using AdoptAMonsterSite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AdoptAMonsterSite.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251027015705_RemovingMonsterIdFromUsers")]
+    partial class RemovingMonsterIdFromUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -99,10 +102,6 @@ namespace AdoptAMonsterSite.Data.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
-                    b.Property<string>("ApplicationUserID")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(450)");
-
                     b.Property<string>("Description")
                         .HasColumnType("nvarchar(max)");
 
@@ -117,8 +116,6 @@ namespace AdoptAMonsterSite.Data.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
-
-                    b.HasIndex("ApplicationUserID");
 
                     b.HasIndex("Name")
                         .IsUnique();
@@ -261,17 +258,6 @@ namespace AdoptAMonsterSite.Data.Migrations
                     b.HasKey("UserId", "LoginProvider", "Name");
 
                     b.ToTable("AspNetUserTokens", (string)null);
-                });
-
-            modelBuilder.Entity("AdoptAMonsterSite.Models.Monster", b =>
-                {
-                    b.HasOne("AdoptAMonsterSite.Models.ApplicationUser", "ApplicationUser")
-                        .WithMany()
-                        .HasForeignKey("ApplicationUserID")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("ApplicationUser");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
--- AdoptAMonsterSite/Models/User.cs
+++ AdoptAMonsterSite/Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AdoptAMonsterSite.Models;
 
@@ -7,5 +8,4 @@ public class ApplicationUser : IdentityUser
     //Custom Fields can go here in the future
     public required string DisplayName { get; set; } = string.Empty;
 
-    public int AdoptedMonsterID { get; set;} = -1;
 }
