282d9f079216218be75cf1cdc6c0ee019f451803
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2025-12-08 15:32:02 -0800

    JOE! LOOK AT ME!

--- AdoptAMonsterSite/Program.cs
+++ AdoptAMonsterSite/Program.cs
@@ -46,14 +46,13 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+//app.MapStaticAssets();
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
 
 app.Run();
